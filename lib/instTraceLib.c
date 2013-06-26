e8d7fc39355181c7017e939909023b83ab57a8ad
Sam Coulter <sam.w.coulter@gmail.com>
2013-06-26 14:03:21 -0700

    Adding a function to instTraceLib.c to check for system endianness, and then changing printInstTracer() appropriately to properly handle printing data values correctly.

--- lib/instTraceLib.c
+++ lib/instTraceLib.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int littleEndianness = -1;
+
+int littleEndian() {
+	int data;
+	char *ptr;
+	if (littleEndianness == -1) {
+		data = 0x1000;
+		ptr = (char*)&data;
+		if (*ptr == 0x1) {
+			littleEndianness = 0;
+		}
+		if (*ptr == 0x0) {
+			littleEndianness = 1;
+		}
+	}
+	return littleEndianness;
+}
 
 void printInstTracer(long instID, int opcode, int size, char* ptr) {
 	int i;
 	printf("ID: %d\tOPCode: %d\tNumBytes: %d\t Value: ", instID, opcode, size);
-	for (i=0;i<size;i++) {
-		printf("%02hhx", ptr[i]);
+	if (littleEndian()) {
+		for (i=size-1; i>=0; i--) {
+			printf("%02hhx", ptr[i]);
+		}
+	} else {
+		for (i=0; i<size; i++) {
+			printf("%02hhx", ptr[i]);
+		}
 	}
 	printf("\n");
 }
