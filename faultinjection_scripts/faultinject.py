379a4b2ecdd0ecc1e8488184bb7cfc6357c2d2f8
Kevin <kcehvoin@hotmail.com>
2013-06-28 02:56:16 -0700

    updating script

--- faultinjection_scripts/faultinject.py
+++ faultinjection_scripts/faultinject.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+#currently, the c file needs to be in the same directory as the script
+
+
 import sys
 import os
 import getopt
@@ -9,33 +12,42 @@ import signal
 import subprocess
 import shutil
 
-#basedir = "/home/jshwei/Desktop/splash_time_automated"
-# config
-basedir = "/data/experiment/LLFI/NoBrNoStack"
-currdir = basedir + "/daxpy"
-progbin = currdir + "/daxpy"
-timeout = 500
-optionlist = []
-
-optbin = "/data/llvm-2.9_build/Debug+Asserts/bin/opt"
-llibin = "/data/llvm-2.9_build/Debug+Asserts/bin/lli"
-linkbin = "/data/llvm-2.9_build/Debug+Asserts/bin/llvm-link"
-llcbin = "/data/llvm-2.9_build/Debug+Asserts/bin/llc"
+optbin = "/data/llvm-2.9/Debug+Asserts/bin/opt"
+llcbin = "/data/llvm-2.9/Debug+Asserts/bin/llc"
 llvmgcc = "/data/llvm-gcc/bin/llvm-gcc"
-instcountlib = "/data/llvm-2.9_build/Debug+Asserts/lib/LLFIProfiling.so"
-filib = "/data/llvm-2.9_build/Debug+Asserts/lib/LLFI.so"
-profilell = basedir + "/../lib/profiling_lib.ll"
-fill = basedir + "/../lib/fi_random.ll"
-outputdir = currdir + "/prog_output"
-basedir = currdir + "/baseline"
-errordir = currdir + "/error_output"
+llfilib = "/data/llvm-2.9/Release/lib/LLFI.so"
+llfilinklib = "/data/LLFI-experimental-master/lib"
+optionlist = []
+timeout = 500
 
-if not os.path.isdir(outputdir):
-  os.mkdir(outputdir)
-if not os.path.isdir(basedir):
-  os.mkdir(basedir)
-if not os.path.isdir(errordir):
-  os.mkdir(errordir)
+#config is now executed after main
+def config():
+	global inputProg,progbin,outputdir,basedir,errordir
+	#basedir = "/home/jshwei/Desktop/splash_time_automated"
+	# config
+	basedir = "/data/CHO"
+	currdir = basedir + "/"+inputProg
+	progbin = currdir + "/"+inputProg
+	
+	
+
+	#linkbin = "/data/llvm-2.9/Debug+Asserts/bin/llvm-link"
+	#llibin = "/data/llvm-2.9/Debug+Asserts/bin/lli"
+	#profilell = basedir + "/../lib/profiling_lib.ll"
+	#fill = basedir + "/../lib/fi_random.ll"
+	
+	outputdir = currdir + "/prog_output"
+	basedir = currdir + "/baseline"
+	errordir = currdir + "/error_output"
+
+	if not os.path.isdir(currdir):
+		os.mkdir(currdir)
+	if not os.path.isdir(outputdir):
+		os.mkdir(outputdir)
+	if not os.path.isdir(basedir):
+		os.mkdir(basedir)
+	if not os.path.isdir(errordir):
+		os.mkdir(errordir)
 
 
 def execute( execlist):
@@ -72,63 +84,76 @@ def execCompilation( execlist):
   p.wait()
 
 def compileProg():
-  global proflinkfile, filinkfile, mem2regfile
-  mem2regfile = progbin + "-mem2reg.ll"
-  proffile = progbin + "-prof.ll"
-  fifile = progbin + "-fi.ll"
-  proflinkfile = progbin + "-prof-link.ll"
-  filinkfile = progbin + "-fi-link.ll"
-  execlist = [optbin, "-load", instcountlib, '-ProfilingPass', "-profileoption=s", 
-              "-S", '-o', proffile, mem2regfile]
-  execCompilation(execlist)
-  execlist = [optbin, "-load", filib, "-FaultInjectionRandom", '-fioption=nobranch',
-              "-S", '-o', fifile, mem2regfile]
-  execCompilation(execlist)
-  
-  execlist = [linkbin, '-S', '-o', proflinkfile, proffile, profilell]
-  execCompilation(execlist)
-  execlist = [linkbin, '-S', '-o', filinkfile, fifile, fill]
-  execCompilation(execlist)
-  
-  execlist = [llcbin, '-filetype=obj', '-o', proflinkfile + '.o', proflinkfile]
-  execCompilation(execlist)
-  execlist = [llcbin, '-filetype=obj', '-o', filinkfile + '.o', filinkfile]
-  execCompilation(execlist)
-  execlist = [llvmgcc, '-o', proflinkfile + '.exe', proflinkfile + '.o']
-  execCompilation(execlist)
-  execlist = [llvmgcc, '-o', filinkfile + '.exe', filinkfile + '.o']
-  execCompilation(execlist)
+	global proffile, fifile, mem2regfile, inputProg,progbin
+	inputllfile = progbin + ".ll"
+	mem2regfile = progbin + "-mem2reg.ll"
+	mem2reg2file = progbin + "-mem2reg2.ll"
+	proffile = progbin + "-prof.ll"
+	fifile = progbin + "-fi.ll"
+
+
+  #TODO -S needs to be an option
+	execlist = [llvmgcc,'-emit-llvm','-S', '-o', inputllfile, inputProg + '.c']
+	execCompilation(execlist)
+	execlist = [optbin, "-mem2reg", '-S', '-o', mem2regfile, inputllfile]
+	execCompilation(execlist)
 	
+
+	execlist = [optbin, '-load', llfilib, '-genllfiindexpass', '-o',mem2reg2file, mem2regfile]
+	execCompilation(execlist)
+	#below 2 commands are specifically for branch
+	execlist = [optbin, '-load', llfilib, '-profilingpass','-S','-insttype','-includeinst=icmp', '-o', proffile, mem2reg2file]
+	execCompilation(execlist)
+	execlist = [optbin, '-load', llfilib, "-faultinjectionpass",'-S','-insttype','-includeinst=icmp', '-o', fifile, mem2reg2file]
+	execCompilation(execlist)
+
+
+	execlist = [llcbin, '-filetype=obj', '-o', proffile + '.o', proffile]
+	execCompilation(execlist)
+	execlist = [llcbin, '-filetype=obj', '-o', fifile + '.o', fifile]
+	execCompilation(execlist)
+
+
+	execlist = [llvmgcc, '-o', proffile + '.exe', proffile + '.o', '-L'+llfilinklib , '-lllfi']
+	execCompilation(execlist)
+	execlist = [llvmgcc, '-o', fifile + '.exe', fifile + '.o', '-L'+llfilinklib , '-lllfi']
+	execCompilation(execlist)
+
 def main():
+	config()
   #clear previous output
-  compileProg()
-  global run_number, optionlist, outputfile, proflinkfile, filinkfile
-  # baseline
-  outputfile = basedir + "/golden_output"
-  execlist = [ proflinkfile + '.exe']
-  execlist.extend(optionlist)
-  execute(execlist)
-  # fault injection
-  for index in range(0, run_number):
-    outputfile = outputdir + "/outputfile-" + str(index)
-    errorfile = errordir + "/errorfile-" + str(index)
-    execlist = [filinkfile + '.exe']
-    execlist.extend(optionlist)
-    ret = execute(execlist)
-    if ret == "timed-out":
-      error_File = open(errorfile, 'w')
-      error_File.write("Program hang\n")
-      error_File.close()
-    elif int(ret) < 0:
-      error_File = open(errorfile, 'w')
-      error_File.write("Program crashed, terminated by the system, return code " + ret + '\n')
-      error_File.close()
-    elif int(ret) > 0:
-      error_File = open(errorfile, 'w')
-      error_File.write("Program crashed, terminated by itself, return code " + ret + '\n')
-      error_File.close()
+	compileProg()
+	#TEMP
+	#exit(1)
+	global run_number, optionlist, outputfile, proffile, fifile
+	# baseline
+	outputfile = basedir + "/golden_output"
+	execlist = [ proffile + '.exe']
+	execlist.extend(optionlist)
+	execute(execlist)
+	# fault injection
+	for index in range(0, run_number):
+		outputfile = outputdir + "/outputfile-" + str(index)
+		errorfile = errordir + "/errorfile-" + str(index)
+		execlist = [fifile + '.exe']
+		execlist.extend(optionlist)
+		ret = execute(execlist)
+		if ret == "timed-out":
+			error_File = open(errorfile, 'w')
+			error_File.write("Program hang\n")
+			error_File.close()
+		elif int(ret) < 0:
+			error_File = open(errorfile, 'w')
+			error_File.write("Program crashed, terminated by the system, return code " + ret + '\n')
+			error_File.close()
+		elif int(ret) > 0:
+			error_File = open(errorfile, 'w')
+			error_File.write("Program crashed, terminated by itself, return code " + ret + '\n')
+			error_File.close()
 if __name__=="__main__":
-  global run_number
-  assert len(sys.argv) == 2 and "Format: prog fi_number"
-  run_number = int(sys.argv[1])
+  global run_number, inputProg
+  assert len(sys.argv) == 3 and "Format: prog fi_number"
+  cfile = sys.argv[1]
+  inputProg = cfile.split('.')[0]#the name of input program
+  run_number = int(sys.argv[2])
   main()
