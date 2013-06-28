490efa3214f048b53adc7125d5a7bd765fa7619c
Kevin <kcehvoin@hotmail.com>
2013-06-28 05:31:02 -0700

    script now supports a preliminary text interface for compilation options, still need to add fi runtime options and kernel exact bit recreation

--- faultinjection_scripts/faultinject.py
+++ faultinjection_scripts/faultinject.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
 #currently, the c file needs to be in the same directory as the script
+#basedir is actually the directory with the script and c file
+#curdir is where the compilation and execution files will be stored
+
+#To run this, please change optbin,llcbin,llvmgcc,llfilib,llfilinklib, and basedir to match your system
 
 
 import sys
@@ -21,6 +25,7 @@ optionlist = []
 timeout = 500
 
 #config is now executed after main
+################################################################################
 def config():
 	global inputProg,progbin,outputdir,basedir,errordir
 	#basedir = "/home/jshwei/Desktop/splash_time_automated"
@@ -49,7 +54,7 @@ def config():
 	if not os.path.isdir(errordir):
 		os.mkdir(errordir)
 
-
+################################################################################
 def execute( execlist):
 	#print "Begin"
 	#inputFile = open(inputfile, "r")
@@ -76,12 +81,14 @@ def execute( execlist):
   return "timed-out"
 	#should never go here	
   sys.exit(syscode)
-
+################################################################################
 
 def execCompilation( execlist):
-  print ' '.join(execlist)
-  p = subprocess.Popen(execlist)
-  p.wait()
+	print ' '.join(execlist)
+	p = subprocess.Popen(execlist)
+	p.wait()
+
+################################################################################
 
 def compileProg():
 	global proffile, fifile, mem2regfile, inputProg,progbin
@@ -91,6 +98,81 @@ def compileProg():
 	proffile = progbin + "-prof.ll"
 	fifile = progbin + "-fi.ll"
 
+	###Instruction selection method
+	while 1:
+		userInput = raw_input("\nInject faults by ... \n1)Instruction type\n2)Custom instruction selector\n")
+		if userInput == '1':
+			compileOptions = ['-insttype']
+			break
+		elif userInput == '2':
+			compileOptions = ['-custominstselector']
+			break
+		else:
+			print ("\n\nERROR. Please select one of the presented options\n")
+
+
+	if userInput == '1':
+		#include list
+		print ("\nWhich instructions should faults be injected to (ie. all,br,ret)\
+														\nPlease enter one at a time. Press Return on empty line to proceed.\n")
+		while 1:
+			userInput = raw_input("")
+			if userInput=='':
+				break
+			else:
+				compileOptions.append('-includeinst='+userInput)
+
+		print ("\nWhich instructions should NOT faults be injected to (ie. all,br,ret)\
+														\nPlease enter one at a time. Press Return on empty line to proceed.\n")
+		#exclude list
+		while 1:
+			userInput = raw_input("")
+			if userInput=='':
+				break
+			else:
+				compileOptions.append('-excludeinst='+userInput)
+	elif userInput =='2':
+		userInput = raw_input("Please enter the name of the custom instruction selector (ie. onlymain)\n")
+		compileOptions.append('-fiinstselectorname='+userInput)
+	###Register selection method
+	while 1:
+		userInput = raw_input("\nInject faults by ... \n1)Register location\n2)Custom registor selector\n")
+		if userInput == '1':
+			compileOptions.append('-regloc')
+			break
+		elif userInput == '2':
+			compileOptions.append('-customregselector')
+			break
+		else:
+			print ("\n\nERROR. Please select one of the presented options\n")
+
+	if userInput == '1':
+		#register location
+		print ("\nWhich register should faults be injected to (ie. dstreg/allsrcreg/srcreg1)\n")
+		while 1:
+			userInput = raw_input("")
+			if userInput=='':
+				print("ERROR, Please enter the target register");
+			else:
+				compileOptions.append('-'+userInput)
+				break
+	elif userInput =='2':
+		userInput = raw_input("Please enter the name of the custom register selector (ie. onlyconstint)\n")
+		compileOptions.append('-firegselectorname='+userInput)
+
+	###Trace selection 
+	while 1:
+		userInput = raw_input("\nSelect trace method ... \n1)Forward\n2)Backward\n")
+		if userInput == '1':
+			compileOptions.append('-includeforwardtrace')
+			break
+		elif userInput == '2':
+			compileOptions.append('-includebackwardtrace')
+			break
+		elif userInput == '':
+			break
+		else:
+			print ("\n\nERROR. Please select one of the presented options\n")
 
   #TODO -S needs to be an option
 	execlist = [llvmgcc,'-emit-llvm','-S', '-o', inputllfile, inputProg + '.c']
@@ -101,10 +183,17 @@ def compileProg():
 
 	execlist = [optbin, '-load', llfilib, '-genllfiindexpass', '-o',mem2reg2file, mem2regfile]
 	execCompilation(execlist)
-	#below 2 commands are specifically for branch
-	execlist = [optbin, '-load', llfilib, '-profilingpass','-S','-insttype','-includeinst=icmp', '-o', proffile, mem2reg2file]
+	
+	execlist = [optbin, '-load', llfilib, '-profilingpass','-S']
+	execlist2 = ['-o', proffile, mem2reg2file]
+	execlist.extend(compileOptions)
+	execlist.extend(execlist2)
 	execCompilation(execlist)
-	execlist = [optbin, '-load', llfilib, "-faultinjectionpass",'-S','-insttype','-includeinst=icmp', '-o', fifile, mem2reg2file]
+
+	execlist = [optbin, '-load', llfilib, "-faultinjectionpass",'-S']
+	execlist2 = ['-o', fifile, mem2reg2file]
+	execlist.extend(compileOptions)
+	execlist.extend(execlist2)
 	execCompilation(execlist)
 
 
@@ -118,14 +207,15 @@ def compileProg():
 	execCompilation(execlist)
 	execlist = [llvmgcc, '-o', fifile + '.exe', fifile + '.o', '-L'+llfilinklib , '-lllfi']
 	execCompilation(execlist)
-
+################################################################################
 def main():
 	config()
   #clear previous output
+	global run_number, optionlist, outputfile, proffile, fifile
+		
 	compileProg()
 	#TEMP
-	#exit(1)
-	global run_number, optionlist, outputfile, proffile, fifile
+	exit(1)
 	# baseline
 	outputfile = basedir + "/golden_output"
 	execlist = [ proffile + '.exe']
