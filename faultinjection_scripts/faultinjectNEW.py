65626329087aff88b93a5cfd0f25b7f73809e545
Kevin <kcehvoin@hotmail.com>
2013-07-07 21:43:19 -0700

    input.yaml takes new parameters, faultinjectNew.py handles input deletion and output conflicts for Bzip2 so far

--- faultinjection_scripts/faultinjectNEW.py
+++ faultinjection_scripts/faultinjectNEW.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python
 
-#currently, the c file needs to be in the same directory as the script
-#basedir is actually the directory with the script and c file
+#currently, the c file(s) needs to be in the same directory as the script
+#basedir is actually the directory with the script and c file(s)
 #curdir is where the compilation and execution files will be stored
 
 #To run this, please change optbin,llcbin,llvmgcc,llfilib,llfilinklib, and basedir to match your system
-
+# sys.path.append() a path that points to your local yaml library
 
 import sys
 sys.path.append('/ubc/ece/home/kp/ugrads/v7c7/lib/python/')
 import yaml
 import os
+import re
 import getopt
 import time
 import random
 import signal
 import subprocess
 import shutil
+import random
 
 optbin = "/data/llvm-2.9/Debug+Asserts/bin/opt"
 llcbin = "/data/llvm-2.9/Debug+Asserts/bin/llc"
+llvmlink = "/data/llvm-2.9/Debug+Asserts/bin/llvm-link"
 llvmgcc = "/data/llvm-gcc/bin/llvm-gcc"
 llfilib = "/data/llvm-2.9/Release/lib/LLFI.so"
 llfilinklib = "/data/LLFI-experimental-master/lib"
@@ -32,13 +35,12 @@ with open('input.yaml','r') as f:
 #config is now executed after main
 ################################################################################
 def config():
-	global inputProg,progbin,outputdir,basedir,errordir
+	global inputProg,progbin,inputdir,outputdir,basedir,errordir
 	#basedir = "/home/jshwei/Desktop/splash_time_automated"
 	# config
-	basedir = "/data/CHO"
+	basedir = os.path.dirname(os.path.abspath(__file__)) #path of directory where script is running on
 	currdir = basedir + "/"+inputProg
 	progbin = currdir + "/"+inputProg
-	
 	
 
 	#linkbin = "/data/llvm-2.9/Debug+Asserts/bin/llvm-link"
@@ -46,10 +48,13 @@ def config():
 	#profilell = basedir + "/../lib/profiling_lib.ll"
 	#fill = basedir + "/../lib/fi_random.ll"
 	
+	inputdir = currdir + "/prog_input"
 	outputdir = currdir + "/prog_output"
 	basedir = currdir + "/baseline"
 	errordir = currdir + "/error_output"
 
+	if not os.path.isdir(inputdir):
+		os.mkdir(inputdir)
 	if not os.path.isdir(currdir):
 		os.mkdir(currdir)
 	if not os.path.isdir(outputdir):
@@ -58,14 +63,15 @@ def config():
 		os.mkdir(basedir)
 	if not os.path.isdir(errordir):
 		os.mkdir(errordir)
-
+		
 ################################################################################
 def execute( execlist):
 	#print "Begin"
 	#inputFile = open(inputfile, "r")
   global outputfile
   print ' '.join(execlist)
-  #print outputfile
+  #get state of directory
+  dirSnapshot()
   p = subprocess.Popen(execlist, stdout = subprocess.PIPE)
   elapsetime = 0
   while (elapsetime < timeout):
@@ -73,11 +79,13 @@ def execute( execlist):
     time.sleep(1)
     #print p.poll()
     if p.poll() is not None:
+      moveOutput()
       print "\t program finish", p.returncode
       print "\t time taken", elapsetime
       outputFile = open(outputfile, "w")
       outputFile.write(p.communicate()[0])
       outputFile.close()
+      replenishInput() #for cases where program deletes input or alters them each run
       #inputFile.close()
       return str(p.returncode)
   #inputFile.close()
@@ -161,27 +169,41 @@ def readCompileOption():
 
 	###Trace selection 
 	if "trace" in cOpt:
-		if cOpt["trace"] == 'forward':
-			compileOptions.append('-includeforwardtrace')
-		elif cOpt["trace"] == 'backward':
-			compileOptions.append('-includebackwardtrace')
-		else:
-			print ("\n\nERROR in input.yaml. Invalid value for trace. (forward/backward allowed)\n")
-			exit(1)
+		for trace in cOpt["trace"]:
+			if trace == 'forward':
+				compileOptions.append('-includeforwardtrace')
+			elif trace == 'backward':
+				compileOptions.append('-includebackwardtrace')
+			else:
+				print ("\n\nERROR in input.yaml. Invalid value for trace. (forward/backward allowed)\n")
+				exit(1)
 
 ################################################################################
 
 def compileProg():
-	global proffile, fifile, mem2regfile, inputProg,progbin,compileOptions
+	global proffile, fifile, mem2regfile,progbin,compileOptions
 	inputllfile = progbin + ".ll"
 	mem2regfile = progbin + "-mem2reg.ll"
 	mem2reg2file = progbin + "-mem2reg2.ll"
 	proffile = progbin + "-prof.ll"
 	fifile = progbin + "-fi.ll"
 
+	#Loop through directory for all c, cc, cpp files
+	srcFiles = [ each for each in os.listdir('.') if each.endswith('.c') or each.endswith('.cc') or each.endswith('.cpp')] 
+
   #TODO -S needs to be an option
-	execlist = [llvmgcc,'-emit-llvm','-S', '-o', inputllfile, inputProg + '.c']
-	execCompilation(execlist)
+	if len(srcFiles) ==1:
+		execlist = [llvmgcc,'-emit-llvm','-S', '-o', inputllfile, srcFiles[0]]
+		execCompilation(execlist)
+	elif len(srcFiles) >1:
+		for  ii, src in enumerate(srcFiles):
+			execlist = [llvmgcc,'-emit-llvm','-S', '-o', progbin +str(ii)+".ll", src]
+			execCompilation(execlist)
+		execlist = [llvmlink, '-S','-o',inputllfile]
+		for ii, src in enumerate(srcFiles):
+			execlist.append(progbin+str(ii)+".ll")
+		execCompilation(execlist)
+
 	execlist = [optbin, "-mem2reg", '-S', '-o', mem2regfile, inputllfile]
 	execCompilation(execlist)
 	
@@ -204,6 +226,8 @@ def compileProg():
 
 	execlist = [llcbin, '-filetype=obj', '-o', proffile + '.o', proffile]
 	execCompilation(execlist)
+
+
 	execlist = [llcbin, '-filetype=obj', '-o', fifile + '.o', fifile]
 	execCompilation(execlist)
 
@@ -213,19 +237,61 @@ def compileProg():
 	execlist = [llvmgcc, '-o', fifile + '.exe', fifile + '.o', '-L'+llfilinklib , '-lllfi']
 	execCompilation(execlist)
 ################################################################################
+def readCycles():
+	global totalcycles
+	profinput= open("llfi.stat.prof.txt")
+	while 1:
+		line = profinput.readline()
+		if line.strip():
+			if line[0] == 't':
+				label, totalcycles = line.split("=")
+				break
+################################################################################
+def storeInputFiles():
+	global inputList
+	inputList=[]
+	for opt in optionlist:
+		if os.path.isfile(opt):#stores all files in inputList and copy over to inputdir
+			shutil.copy2(opt, inputdir+'/'+opt)
+			inputList.append(opt)
+
+################################################################################
+def replenishInput():
+	for each in inputList:
+		if not os.path.isfile(each):#copy deleted inputfiles back to basedir
+			shutil.copy2(inputdir+'/'+each,each)################################################################################
+def moveOutput():
+	for each in[file for file in os.listdir(".")]:
+		if each not in dirBefore and each !="llfi.stat.prof.txt":
+			flds = each.split(".")
+			newName = '.'.join(flds[0:-1])
+			newName+='.'+run_id+'.'+flds[-1]
+			os.rename(each,outputdir+'/'+newName)
+################################################################################
+def dirSnapshot():
+	global dirBefore
+	dirBefore=[]
+	for each in[file for file in os.listdir(".")]:
+		dirBefore.append(each)
+
+################################################################################
 def main():
 	config()
   #clear previous output
-	global optionlist, outputfile, proffile, fifile
+	global optionlist, outputfile, proffile, fifile, totalcycles,inputList,run_id
 	readCompileOption()
 	compileProg()
-	
+	storeInputFiles()
 	# baseline
 	outputfile = basedir + "/golden_output"
-	execlist = [ proffile + '.exe']
+	execlist = [ progbin + "-prof.ll" + '.exe']#TODO change back to proffile 
 	execlist.extend(optionlist)
+	run_id=''#execute requires run_id to be defined
 	execute(execlist)
-	
+
+	# get total num of cycles
+	readCycles()
+
 	#Setup each config file and it's corresponding run_number
 	rOpt = doc["runOption"]
 	for ii, run in enumerate(rOpt):
@@ -235,30 +301,43 @@ def main():
 		else:
 			run_number=run["run"]["numOfRuns"]
 			#write new fi config file according to input.yaml
-			ficonfig_File = open("llfi.config.fi.txt", 'w')
+			fi_type=fi_cycle=fi_index=fi_reg_index=fi_bit=''
 			if "fi_type" in run["run"]:
 				fi_type=run["run"]["fi_type"]
-				ficonfig_File.write("fi_type="+fi_type+'\n')
 			if "fi_cycle" in run["run"]:
 				fi_cycle=run["run"]["fi_cycle"]
-				ficonfig_File.write("fi_cycle="+str(fi_cycle)+'\n')
 			if "fi_index" in run["run"]:
-				fi_index=run["run"]["fi_index"]
-				ficonfig_File.write("fi_index="+str(fi_index)+'\n')
+				fi_index=run["run"]["fi_index"]			
 			if "fi_reg_index" in run["run"]:
-				fi_reg_index=run["run"]["fi_reg_index"]
-				ficonfig_File.write("fi_reg_index="+str(fi_reg_index)+'\n')
+				fi_reg_index=run["run"]["fi_reg_index"]		
 			if "fi_bit" in run["run"]:
 				fi_bit=run["run"]["fi_bit"]
-				ficonfig_File.write("fi_bit="+str(fi_bit)+'\n')
-		
-			ficonfig_File.close()			
 
 			# fault injection
 			for index in range(0, run_number):
-				outputfile = outputdir + "/outputfile-" + "run-"+str(ii)+"-"+str(index)
-				errorfile = errordir + "/errorfile-" + "run-"+str(ii)+"-"+str(index)
-				execlist = [fifile + '.exe']
+				run_id = str(ii)+"-"+str(index)
+				outputfile = outputdir + "/outputfile-" + "run-"+run_id
+				errorfile = errordir + "/errorfile-" + "run-"+run_id
+				execlist = [progbin + "-fi.ll" + '.exe']#TODO change back to fifile
+	
+				ficonfig_File = open("llfi.config.fi.txt", 'w')
+				
+				if fi_type:
+					ficonfig_File.write("fi_type="+fi_type+'\n')
+				if fi_cycle:
+					pass#Do nothing, fi_cycle is already storing user input specific cycle
+				else:
+					fi_cycle=random.randint(1, int(totalcycles))#randomly choose cycle if it isnt specified
+				ficonfig_File.write("fi_cycle="+str(fi_cycle)+'\n')
+				if fi_index:
+					ficonfig_File.write("fi_index="+str(fi_index)+'\n')
+				if fi_reg_index:				
+					ficonfig_File.write("fi_reg_index="+str(fi_reg_index)+'\n')
+				if fi_bit:				
+					ficonfig_File.write("fi_bit="+str(fi_bit)+'\n')
+
+				ficonfig_File.close()
+			
 				execlist.extend(optionlist)
 				ret = execute(execlist)
 				if ret == "timed-out":
@@ -274,11 +353,12 @@ def main():
 					error_File.write("Program crashed, terminated by itself, return code " + ret + '\n')
 					error_File.close()
 
-
+################################################################################
 
 if __name__=="__main__":
   global inputProg
-  assert len(sys.argv) == 2 and "Format: prog fi_number"
-  cfile = sys.argv[1]
-  inputProg = cfile.split('.')[0]#the name of input program
+  inputProg = doc["setUp"]["targetDirectoryName"]#the name of input program
+  for ii,arg in enumerate(sys.argv):
+    if ii: #add all commandline arguments to optionlist except the first one which is the this script's name
+      optionlist.append(arg)
   main()
