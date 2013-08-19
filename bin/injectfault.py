078fe506c8d8bb3fa71ad8943456c44fa12af706
Jiesheng Wei <jwei@ece.ubc.ca>
2013-08-19 15:14:55 -0700

    Bug fix, move generated files out and restore input even when the program hangs

--- bin/injectfault.py
+++ bin/injectfault.py
@@ -139,6 +139,8 @@ def execute( execlist):
   #inputFile.close()
   print "\tParent : Child timed out. Cleaning up ... "
   p.kill()
+  moveOutput()
+  replenishInput()
   return "timed-out"
 
 ################################################################################
