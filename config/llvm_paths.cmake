dc1ec1ebc5454e6216505cfe4b027459b9cd384d
Maryam <raiyat@yahoo.com>
2014-03-13 03:41:10 -0700

    new test program added

--- config/llvm_paths.cmake
+++ config/llvm_paths.cmake
@@ -1,2 +1,2 @@
-set(LLVM_DST_ROOT /ubc/ece/home/am/grads/raiyat/project/llvm_build)
-set(LLVM_SRC_ROOT /ubc/ece/home/am/grads/raiyat/project/llvm-2.9)
+set(LLVM_DST_ROOT /ubc/ece/home/kp/grads/raiyat/newproject/llvm_build)
+set(LLVM_SRC_ROOT /ubc/ece/home/kp/grads/raiyat/newproject/llvm-2.9)
--- config/llvm_paths.py
+++ config/llvm_paths.py
@@ -1,3 +1,3 @@
-LLVM_DST_ROOT = "/ubc/ece/home/am/grads/raiyat/project/llvm_build"
-LLVM_SRC_ROOT = "/ubc/ece/home/am/grads/raiyat/project/llvm-2.9"
-LLVM_GXX_BIN_DIR = "/ubc/ece/home/am/grads/raiyat/llvm-gcc/bin"
+LLVM_DST_ROOT = "/ubc/ece/home/kp/grads/raiyat/newproject/llvm_build"
+LLVM_SRC_ROOT = "/ubc/ece/home/kp/grads/raiyat/newproject/llvm-2.9"
+LLVM_GXX_BIN_DIR = "/ubc/ece/home/kp/grads/raiyat/newproject/llvm-gcc/bin"
