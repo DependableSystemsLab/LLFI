a2062bd1c5c9e941e9293f687d0d1039fa427ddc
Qining Lu <lqn.anthony@gmail.com>
2014-10-20 18:11:19 -0700

    minor

--- runtime_lib/FaultInjectionLib.c
+++ runtime_lib/FaultInjectionLib.c
@@ -178,7 +178,7 @@ bool preFunc(long llfi_index, unsigned opcode, unsigned my_reg_index,
 
 void injectFunc(long llfi_index, unsigned size, 
                 char *buf, unsigned my_reg_index) {
-  fprintf(stderr, "MSG: injectFunc() being called\n");
+  fprintf(stderr, "MSG: injectFunc() has being called\n");
   if (! fiFlag) return;
   start_tracing_flag = TRACING_FI_RUN_FAULT_INSERTED; //Tell instTraceLib that we have injected a fault
 
