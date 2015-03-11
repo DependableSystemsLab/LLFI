e2ce3c8ad83573f43302e65bfd454ac10cce97f8
karfair <sittipol_tribunyatkul@hotmail.com>
2015-03-10 23:23:41 -0700

    fixed code formatting

--- gui/application/MyThread.java
+++ gui/application/MyThread.java
@@ -17,97 +17,96 @@ import javafx.stage.Stage;
 /**
  * The Fault Injection Thread. Called by the ProgressBarController class
  */
-public class MyThread extends Thread
-{
-	
-   private int startIdx, nThreads, maxIdx;
-   private boolean errorFlag;
-   public MyThread()
-   {
-   }
-   //
-   @Override
-   public void run()
-   {
-	   
-	  
-	   Parent root;
-	   try{
-		   Controller.console = new ArrayList<String>(); 
-		   
-		   String execName = "bin/injectfault "+Controller.currentProgramFolder+"/llfi/"+Controller.currentProgramFolder+"-faultinjection.exe "+Controller.inputString;	
+public class MyThread extends Thread {
 
-		   ProcessBuilder p = new ProcessBuilder("/bin/tcsh","-c",Controller.llfibuildPath + execName);
-		   Controller.console.add("$ "+Controller.llfibuildPath + execName+ "\n");
-		   p.redirectErrorStream(true);
-		   Process pr = p.start();
-		    
-			BufferedReader in1 = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-		    String line1;
-		    while ((line1 = in1.readLine()) != null) {
-		    	 Controller.console.add(line1+"\n");
-		    	Controller.errorString.add(line1+"\n");
-		    	if(line1.contains("error")||line1.contains("Error")||line1.contains("ERROR"))
-		    		errorFlag= true;
-		    	
-		        
-		    }
-		    pr.waitFor();
-		   in1.close();
-		   pr.destroy(); 
-		   javafx.application.Platform.runLater(new Runnable() {
-			   @Override
-		        public void run() {
-				   try{
-				   if(errorFlag == true)
-					  {
-					   //progressBar.visibleProperty().unbind();
-					   //progressBar.setVisible(false);
-					   //indicator.setVisible(false);
-						  errorFlag = false;
-						 
-						  
-						    Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("application/ErrorDisplay.fxml"));
-						    Stage stage = new Stage();
-					        stage.setTitle("Error");
-					        stage.setScene(new Scene(root, 450, 100));
-					        stage.show();
-					        
-					  }
-					  else
-					  {
-						  Controller.errorString = new ArrayList<>();
-						  //progressBar.visibleProperty().unbind();
-						  //progressBar.setVisible(false);
-						  //indicator.setVisible(false);
-						 // onGeneratingResultTable();
-						 // generateFaultSummaryGraph();
-						  
-						  //tabBottom.getSelectionModel().select(faultStatus);
-						 // Node  source = (Node)  event.getSource(); 
-						  //Stage stage  = (Stage) source.getScene().getWindow();
-						  //stage.close();
-					  }
-		        }catch (IOException e) {
-		 	       System.err.println("Problem writing to the file statsTest.txt");
-			    }
-			   }
-		   });  
-		    Thread.sleep(1000);
-		  
-	   
-	  
-	   }catch (IOException e) {
-	       System.err.println("Problem writing to the file statsTest.txt");
-	    }
-		catch (InterruptedException e) {
-	    	System.out.println(e);
+	private int startIdx, nThreads, maxIdx;
+	private boolean errorFlag;
+
+	public MyThread() {
+	}
+
+	//
+	@Override
+	public void run() {
+
+		Parent root;
+		try {
+			Controller.console = new ArrayList<String>();
+
+			String execName = "bin/injectfault "
+					+ Controller.currentProgramFolder + "/llfi/"
+					+ Controller.currentProgramFolder + "-faultinjection.exe "
+					+ Controller.inputString;
+
+			ProcessBuilder p = new ProcessBuilder("/bin/tcsh", "-c",
+					Controller.llfibuildPath + execName);
+			Controller.console.add("$ " + Controller.llfibuildPath + execName
+					+ "\n");
+			p.redirectErrorStream(true);
+			Process pr = p.start();
+
+			BufferedReader in1 = new BufferedReader(new InputStreamReader(
+					pr.getInputStream()));
+			String line1;
+			while ((line1 = in1.readLine()) != null) {
+				Controller.console.add(line1 + "\n");
+				Controller.errorString.add(line1 + "\n");
+				if (line1.contains("error") || line1.contains("Error")
+						|| line1.contains("ERROR"))
+					errorFlag = true;
+
+			}
+			pr.waitFor();
+			in1.close();
+			pr.destroy();
+			javafx.application.Platform.runLater(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						if (errorFlag == true) {
+							// progressBar.visibleProperty().unbind();
+							// progressBar.setVisible(false);
+							// indicator.setVisible(false);
+							errorFlag = false;
+
+							Parent root = FXMLLoader.load(getClass()
+									.getClassLoader().getResource(
+											"application/ErrorDisplay.fxml"));
+							Stage stage = new Stage();
+							stage.setTitle("Error");
+							stage.setScene(new Scene(root, 450, 100));
+							stage.show();
+
+						} else {
+							Controller.errorString = new ArrayList<>();
+							// progressBar.visibleProperty().unbind();
+							// progressBar.setVisible(false);
+							// indicator.setVisible(false);
+							// onGeneratingResultTable();
+							// generateFaultSummaryGraph();
+
+							// tabBottom.getSelectionModel().select(faultStatus);
+							// Node source = (Node) event.getSource();
+							// Stage stage = (Stage)
+							// source.getScene().getWindow();
+							// stage.close();
+						}
+					} catch (IOException e) {
+						System.err
+								.println("Problem writing to the file statsTest.txt");
+					}
+				}
+			});
+			Thread.sleep(1000);
+
+		} catch (IOException e) {
+			System.err.println("Problem writing to the file statsTest.txt");
+		} catch (InterruptedException e) {
+			System.out.println(e);
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	   
-   
-   }
-   
-   
+
+	}
+
 }
--- gui/application/ProgressBarController.java
+++ gui/application/ProgressBarController.java
@@ -1,6 +1,5 @@
 package application;
 
- 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -23,10 +22,8 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import application.Controller;
 
+public class ProgressBarController implements Initializable {
 
-
-public class ProgressBarController implements Initializable{
-	
 	public int totalRunCount = 0;
 	public int currentCount = 0;
 	@FXML
@@ -35,146 +32,140 @@ public class ProgressBarController implements Initializable{
 	private ProgressIndicator indicator;
 	@FXML
 	private TextField runText;
+
 	@FXML
-	private void onClickOkHandler(ActionEvent event){
-		
-		
-		Node  source = (Node)  event.getSource(); 
-	    Stage stage  = (Stage) source.getScene().getWindow();
-	    stage.close();
+	private void onClickOkHandler(ActionEvent event) {
+
+		Node source = (Node) event.getSource();
+		Stage stage = (Stage) source.getScene().getWindow();
+		stage.close();
 	}
-	
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-	    // TODO
-	try{
-		
-	FileReader inputFile = new FileReader(Controller.currentProgramFolder+"/input.yaml");
-	BufferedReader bufferReader = new BufferedReader(inputFile);
-    String line;
- 
-   while ((line = bufferReader.readLine()) != null)   {
- 	if(line.contains("numOfRuns"))
- 	{
- 		totalRunCount = totalRunCount+Integer.parseInt(line.split(":")[1].trim());
- 	}
- 	}
-   runText.setText(Integer.toString(totalRunCount));
-	/*	//System.out.println("\ntotalRunCount ="+totalRunCount);
-		File file = new File(Controller.currentProgramFolder+"/llfi/llfi_stat_output/");
-		 
-		if(file.isDirectory()){
-	 
-			if(file.list().length>0){
-				currentCount = file.listFiles().length;
+		// TODO
+		try {
+
+			FileReader inputFile = new FileReader(
+					Controller.currentProgramFolder + "/input.yaml");
+			BufferedReader bufferReader = new BufferedReader(inputFile);
+			String line;
+
+			while ((line = bufferReader.readLine()) != null) {
+				if (line.contains("numOfRuns")) {
+					totalRunCount = totalRunCount
+							+ Integer.parseInt(line.split(":")[1].trim());
+				}
 			}
-		}*/
-  	 // if(currentCount != totalRunCount)
-  		 
-  		  //System.out.println("currentCount = "+currentCount);
-			
-		//progressBar.visibleProperty().bind(event..progressProperty().lessThan(1));
-		//
-		//tabBottom.getSelectionModel().select(faultStatus);
-		progressBar.setVisible(true);
-		//indicator.setVisible(true);
-		//indicator.setProgress(-1);
-		//progressBar.setProgress(-1);
-		Task<Void> task = new Task<Void>()
-		{
-			 @Override public Void call()
-			 {
-				 
-				 int parts = 0;
-				 int i = 0;
-				 int listCount=0;
-				 String tempName=null;
-				// System.out.println("i = "+i);
-				 while( i <= totalRunCount) {
-			          try {
-			        	  //System.out.println("i1 = "+Controller.currentProgramFolder+"/llfi/llfi_stat_output/");
-			        	  File file = new File(Controller.currentProgramFolder+"/llfi/llfi_stat_output/");
-			        	  listCount=0;
-			      		if(file.isDirectory()){
-			      			File[] listOfFiles = file.listFiles();
-			      			if(listOfFiles.length>0){
-			      				for(int k = 0;k<listOfFiles.length;k++)
-			      				{
-			      					tempName = listOfFiles[k].getName();
-			      					//tempName = "he";
-			      					if(tempName.contains("injectedfaults"))
-						    		{
-			      						//System.out.println(listOfFiles[k].getName());
-			      						listCount++;
-						    		}
-			      					/*if(tempName.equalsIgnoreCase("llfi"));
-			      					{
-			      						System.out.println(listOfFiles[k].getName());
-			      						listCount++;
-			      					}*/
-			      					
-			      					
-			      				}
-			      		    currentCount = listCount;
-			      		  //System.out.println(currentCount);
-			         	 // if(currentCount != totalRunCount)
-			         		 
-			         		  //System.out.println("currentCount = "+currentCount);
-			      			}
-			      			}
-			        	  
-			        	 // if(currentCount != totalRunCount)
-			        		  i=currentCount;
-			        		  //System.out.println("currentCount = "+currentCount);
-			            Thread.sleep(1000);
-			          } catch (InterruptedException e) {
-			            e.printStackTrace();
-			          }
-			          //System.out.println(i);
-			          updateProgress(i, totalRunCount);
-			            if(i== totalRunCount)
-			          {
-			        	 try {
+			runText.setText(Integer.toString(totalRunCount));
+			/*
+			 * //System.out.println("\ntotalRunCount ="+totalRunCount); File
+			 * file = new
+			 * File(Controller.currentProgramFolder+"/llfi/llfi_stat_output/");
+			 * 
+			 * if(file.isDirectory()){
+			 * 
+			 * if(file.list().length>0){ currentCount = file.listFiles().length;
+			 * } }
+			 */
+			// if(currentCount != totalRunCount)
+
+			// System.out.println("currentCount = "+currentCount);
+
+			// progressBar.visibleProperty().bind(event..progressProperty().lessThan(1));
+			//
+			// tabBottom.getSelectionModel().select(faultStatus);
+			progressBar.setVisible(true);
+			// indicator.setVisible(true);
+			// indicator.setProgress(-1);
+			// progressBar.setProgress(-1);
+			Task<Void> task = new Task<Void>() {
+				@Override
+				public Void call() {
+
+					int parts = 0;
+					int i = 0;
+					int listCount = 0;
+					String tempName = null;
+					// System.out.println("i = "+i);
+					while (i <= totalRunCount) {
+						try {
+							// System.out.println("i1 = "+Controller.currentProgramFolder+"/llfi/llfi_stat_output/");
+							File file = new File(
+									Controller.currentProgramFolder
+											+ "/llfi/llfi_stat_output/");
+							listCount = 0;
+							if (file.isDirectory()) {
+								File[] listOfFiles = file.listFiles();
+								if (listOfFiles.length > 0) {
+									for (int k = 0; k < listOfFiles.length; k++) {
+										tempName = listOfFiles[k].getName();
+										// tempName = "he";
+										if (tempName.contains("injectedfaults")) {
+											// System.out.println(listOfFiles[k].getName());
+											listCount++;
+										}
+										/*
+										 * if(tempName.equalsIgnoreCase("llfi"));
+										 * {
+										 * System.out.println(listOfFiles[k].getName
+										 * ()); listCount++; }
+										 */
+
+									}
+									currentCount = listCount;
+									// System.out.println(currentCount);
+									// if(currentCount != totalRunCount)
+
+									// System.out.println("currentCount = "+currentCount);
+								}
+							}
+
+							// if(currentCount != totalRunCount)
+							i = currentCount;
+							// System.out.println("currentCount = "+currentCount);
 							Thread.sleep(1000);
 						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-			        	 // indicator.setVisible(false);
-			        	 
-			        	 // progressBar.setVisible(false);
-			        	  //onGeneratingResultTable();
-			        	 // generateFaultSummaryGraph();
-			        	 // 
-			        	  
-			        	  Controller.errorString = new ArrayList<>();
-			        	 
-						 
-				           
-			          }
-			        	  
-			        }
-				 return null;
-			 }
-		};
-		
-		  MyThread t1 = new MyThread();
-	    
-	        t1.start();
-	       // Thread.sleep(2000);
-	        progressBar.progressProperty().bind(task.progressProperty());
-            indicator.progressProperty().bind(task.progressProperty());
-	        Thread th = new Thread(task);
-	        th.setDaemon(true);
-	        th.start();
-	        
-	        
-		
-	    
-	}catch (Exception e) {
-	      System.err.println("Problem writing to the file statsTest.txt");
-	   }
+						// System.out.println(i);
+						updateProgress(i, totalRunCount);
+						if (i == totalRunCount) {
+							try {
+								Thread.sleep(1000);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							// indicator.setVisible(false);
 
-}
+							// progressBar.setVisible(false);
+							// onGeneratingResultTable();
+							// generateFaultSummaryGraph();
+							//
+
+							Controller.errorString = new ArrayList<>();
+
+						}
+
+					}
+					return null;
+				}
+			};
+
+			MyThread t1 = new MyThread();
+
+			t1.start();
+			// Thread.sleep(2000);
+			progressBar.progressProperty().bind(task.progressProperty());
+			indicator.progressProperty().bind(task.progressProperty());
+			Thread th = new Thread(task);
+			th.setDaemon(true);
+			th.start();
+
+		} catch (Exception e) {
+			System.err.println("Problem writing to the file statsTest.txt");
+		}
+
+	}
 }
