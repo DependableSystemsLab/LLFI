025e3b70987f6a08fc5b3204691fdeee302f103b
Kenneth <syltaxue@hotmail.com>
2016-09-21 15:34:21 -0700

    Adding libraries

--- web-app/src/js/components/header.js
+++ web-app/src/js/components/header.js
@@ -1,34 +1,18 @@
 var React = require("react");
 
 var Header = React.createClass({
-	getInitialState: function() {
-		return ({active: 'home'});
-	},
-	shouldComponentUpdate: function(nextProps, nextState) {
-		if (this.props !== nextProps) return true;
-		if (this.state && this.state.active !== nextState.active) return true;
-		return false;
-	},
 	render: function() {
-		var homeButtonState = this.state.active === 'home' ? ' active' : '';
 		return (
 			<div className = "header">
-				<div className = "header-container">
-					<div className= "row">
-						<div className = "col-sm-1"></div>
-						<div className = "col-sm-10">
-							<div className = "header-container-context" onClick={this._handleClick.bind(this, 'home')}>
-								<div className="header-buttons-text">Kenneth's Portfolio</div>
-							</div>
-						</div>
-						<div className= "col-sm-1"></div>
+				<div className= "row">
+					<div className = "col-sm-2"></div>
+					<div className = "col-sm-8">
+						<div className="header-message">Welcome to LLFI Web Service </div>
 					</div>
+					<div className= "col-sm-2"></div>
 				</div>
 			</div>
 		);
-	},
-	_handleClick: function(type) {
-		this.setState({active: type});
 	}
 });
 
