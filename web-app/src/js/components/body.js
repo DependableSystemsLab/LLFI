88a968d69956bbc0b22c22cbc73c82cc4f537fa4
Kenneth <syltaxue@hotmail.com>
2016-09-21 15:48:18 -0700

    Added standard/advanced tabs

--- web-app/src/js/components/body.js
+++ web-app/src/js/components/body.js
@@ -5,7 +5,14 @@ var Body = React.createClass({
 	render: function() {
 		return (
 			<div className = "body">
-				body
+				<ul className="nav nav-tabs">
+					<li className="active">
+						<a href="#">Standard</a>
+					</li>
+					<li className="disabled">
+						<a>Advanced</a>
+					</li>
+				</ul>
 			</div>
 		);
 	}
