edc9b4c4bff1729c8589d1c439c1a2fdbaca1750
Kenneth <syltaxue@hotmail.com>
2016-09-27 17:40:35 -0700

    Added file submenu

--- web-app/src/js/components/tools.js
+++ web-app/src/js/components/tools.js
@@ -4,11 +4,17 @@ var React = require("react");
 var Tools = React.createClass({
 	render: function() {
 		return (
-			<div className = "tools">
-				<ul className="nav nav-pills">
-					<li><a>Files</a></li>
-					<li><a>Edit</a></li>
-					<li className="disabled"><a>Help</a></li>
+			<div class = "tools">
+				<ul class="nav nav-pills">
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Files</a>
+						<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+							<li><a tabindex="-1">Open File</a></li>
+							<li><a tabindex="-1">Import Project</a></li>
+						</ul>
+					</li>
+					<li class="disabled"><a>Edit</a></li>
+					<li class="disabled"><a>Help</a></li>
 				</ul>
 			</div>
 		);
