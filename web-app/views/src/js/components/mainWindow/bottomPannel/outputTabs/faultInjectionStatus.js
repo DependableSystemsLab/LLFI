c2a20efbf3cb7e4cf27d04ebb55350cb66e86f9c
Kenneth Song <syltaxue@hotmail.com>
2016-11-06 21:54:34 -0800

    Front-end trace select working

--- web-app/views/src/js/components/mainWindow/bottomPannel/outputTabs/faultInjectionStatus.js
+++ web-app/views/src/js/components/mainWindow/bottomPannel/outputTabs/faultInjectionStatus.js
@@ -6,12 +6,14 @@ var FaultInjectionStatus = React.createClass({
 	mixins: [Reflux.connect(faultInjectionStatusStore,"faultInjectionStatus")],
 	getInitialState: function() {
 		return {
-			faultInjectionStatus: []
+			faultInjectionStatus: [],
+			traceRunNumbers: []
 		};
 	},
 	render: function() {
 		var className = "faultInjectionStatus" + (this.props.shouldDisplay ? "" : " hide");
 		var injectionStatusRows = this.state.faultInjectionStatus.map(function(data, index) {
+			var traceChecked = this.state.traceRunNumbers.includes(data.runOption) ? true: false;
 			return (
 				<tr key={index}>
 					<td>{data.runOption}</td>
@@ -24,12 +26,12 @@ var FaultInjectionStatus = React.createClass({
 					<td>Nil</td>
 					<td>
 						<div class="checkbox">
-							<input type="checkbox" value=""/>
+							<input type="checkbox" id={"checkBoxTrace_" + data.runOption} onClick={this.selectTrace} checked={traceChecked}/>
 						</div>
 					</td>
 				</tr>
 			)
-		});
+		}.bind(this));
 		return (
 			<div class={className}>
 				<table class="table table-hover faultInjectionStatusTable">
@@ -46,7 +48,7 @@ var FaultInjectionStatus = React.createClass({
 							<th class="traceHeader">
 								Trace
 								<div class="checkbox">
-									<label><input type="checkbox" value=""/>Select All</label>
+									<label><input id="checkallTrace" type="checkbox" onClick={this.onClickCheckAll}/>Select All</label>
 								</div>
 							</th>
 						</tr>
@@ -57,6 +59,35 @@ var FaultInjectionStatus = React.createClass({
 				</table>
 			</div>
 		);
+	},
+	selectTrace : function (event) {
+		var selectedRunNumber = parseInt(event.target.id.split("_")[1]);
+		var traceChecked = $("#" + event.target.id).is(":checked");
+		var traceRunNumbers = this.state.traceRunNumbers;
+		if (traceChecked) {
+			// Add the run number if the checkbox is checked
+			traceRunNumbers.push(selectedRunNumber);
+		} else {
+			// Remove the run number if the checked is unchecked
+			var index = traceRunNumbers.indexOf(selectedRunNumber);
+			if (index > -1) traceRunNumbers.splice(index, 1);
+		}
+		this.setState({
+			traceRunNumbers: traceRunNumbers
+		});
+	},
+	onClickCheckAll : function (event) {
+		var traceAllChecked = $("#checkallTrace").is(":checked");
+		var traceRunNumbers = [];
+		// If check all is checked, add all runs, otherwise, remove all runs
+		if (traceAllChecked) {
+			for (var i = 0; i < this.state.faultInjectionStatus.length; i++) {
+				traceRunNumbers.push(this.state.faultInjectionStatus[i].runOption);
+			}
+		}
+		this.setState({
+			traceRunNumbers: traceRunNumbers
+		});
 	}
 });
 
